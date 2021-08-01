03d574df7ad22e34ea9c8d97e637aa703d246172
Ami Drammeh <amidrammeh1@gmail.com>
2021-07-31 22:06:03 -0700

    initial commit

---
+++ java-fundamentals/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+  public static void main(String[] args) {
+    System.out.println("My name is Ami");
+    System.out.println("I am 25");
+    System.out.println("From Seattle");
+  }
+}
